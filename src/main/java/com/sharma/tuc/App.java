dd31c0749f460d008fce8066830d5740dae4bfbb
shawon-sharma <shawon.sharma@tu-clausthal.de>
2023-09-08 09:28:03 +0200

    Initial Commit

---
+++ src/main/java/com/sharma/tuc/App.java
@@ -0,0 +1,13 @@
+package com.sharma.tuc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
